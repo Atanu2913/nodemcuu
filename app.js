02cfd18ffe6a88f21de22ec2acd6dafaaec7f637
Atanu2913 <atanu2913@gmail.com>
2022-03-04 08:17:39 +0530

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { Api } from "./api/index.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+const port = process.env.APP_PORT || 3001;
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api", Api);
 app.get("/", (req, res) => {
   console.log( "Welcome to Nodemcu Log");
 
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
     message: "Welcome to Nodemcu"
   });
 });
-app.listen(3000, function() {
-  console.log(`Express app running on port 3000`);
+app.use("/api", Api);
+
+app.listen(port, () => {
+  console.log(`Express app running on port ${port}`);
 });
